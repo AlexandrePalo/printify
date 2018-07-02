80d076f7a56e4fa7ca7c2014bf1740590bf8bd9b
Alexandre PALO <a-palo@laposte.net>
2018-07-02 21:40:47 +0200

    wip printer actions

--- src/redux/actions/printer.js
+++ src/redux/actions/printer.js
@@ -54,4 +54,66 @@ const connectToPrinter = (port, baudRate) => {
   }
 }
 
-export { getAvailablePorts, connectToPrinter }
+// WIP
+const disconnectFromPrinter = () => {
+  return dispatch => {}
+}
+
+const startPrint = (printerId, file) => {
+  return dispatch => {
+    // Loading state, to send first information
+    dispatch({
+      type: 'START_PRINTING',
+      fetching: true
+    })
+    // Request a print to server
+    const query = `mutation() {}` // TBC
+    const gqlc = new GraphQLClient(endpoint, {
+      headers: { authorization: 'Bearer ' + sessionStorage.jwt }
+    })
+    gqlc
+      .request(endpoint, query, {}) // TBC
+      .then(data => {
+        // Server is ok, and started to print
+        // Start in state
+        dispatch({
+          type: 'START_PRINTING',
+          payload: { id: file.id }
+        })
+      })
+      .catch(err => {
+        // Server is not ok
+        dispatch({
+          type: 'START_PRINTING',
+          error: true,
+          message: 'Error during printing initialization'
+        })
+      })
+  }
+}
+
+const getPrintState = printingId => {
+  return dispatch => {}
+}
+
+const stopPrint = printingId => {
+  return dispatch => {}
+}
+
+const pausePrint = printingId => {
+  return dispatch => {}
+}
+
+const resumePrint = printingId => {
+  return dispatch => {}
+}
+
+export {
+  getAvailablePorts,
+  connectToPrinter,
+  disconnectFromPrinter,
+  startPrint,
+  stopPrint,
+  pausePrint,
+  resumePrint
+}
